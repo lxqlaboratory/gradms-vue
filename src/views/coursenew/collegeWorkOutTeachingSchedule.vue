2625e59cfec46ea0f5614ad992e4d54b849e12af
lixueqingsd <xqli@sdu.edu.cn>
2020-01-28 11:44:14 +0800

    lxq

--- src/views/coursenew/collegeWorkOutTeachingSchedule.vue
+++ src/views/coursenew/collegeWorkOutTeachingSchedule.vue
@@ -1,15 +1,35 @@
 <template>
     <div class="app-container">
-      collegeWorkOutTeachingSchedule
+    <calendar style="height: 800px;"/>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'collegeWorkOutTeachingSchedule'
-  }
+import 'tui-calendar/dist/tui-calendar.css'
+import { Calendar } from '@toast-ui/vue-calendar'
+
+export default {
+    name: 'collegeWorkOutTeachingSchedule',
+    components: {
+        'calendar': Calendar
+    }
+}
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
