30665c7f5ac6e51131a4de49dc7fa2fd9734c361
lixueqingsd <xqli@sdu.edu.cn>
2020-05-02 15:08:30 +0800

    lxq

--- src/views/tutor/recruitTeamCheck.vue
+++ src/views/tutor/recruitTeamCheck.vue
@@ -40,6 +40,15 @@
             </template>
           </el-table-column>
           <el-table-column
+            label="责任专家编号"
+            align="center"
+            color="black"
+          >
+          <template slot-scope="scope">
+              {{ scope.row.leaderNum }}
+            </template>
+          </el-table-column>
+          <el-table-column
             label="责任专家"
             align="center"
             color="black"
@@ -133,13 +142,13 @@ export default {
       })
     },
     doExport(){
-      var filename = "申请信息信息表.xlsx";
+      var filename = "团队申请信息表.xlsx";
       // 工作簿中工作表的名字
       var sheetName = "团队申请信息表";
 
       // head定义了整个xlsx的顺序，里面的内容时json object的key
-      const header = ["teamName","leaderName","perNams", "perName", "tutorName","thesisNum", "uploadTime", "checkTime", "reviewTotal","reviewExpert","reviewResult"];
-      const headerExcel = ["学生类型","专业","学号", "姓名", "导师姓名","论文编号", "上传时间", "导师审核时间", "评阅分数", "评阅专家", "评阅结果"];
+      const header = ["teamName","collegeName","leaderNum","leaderName","perNames", "des"];
+      const headerExcel = ["团队名称","培养单位","责任导师编号", "责任姓名", "团队成员","团队介绍"];
 
       const XlsxPopulate = require('xlsx-populate');
 
@@ -152,7 +161,7 @@ export default {
         ws.name(sheetName);
 
         // Set table name
-        const r = ws.range("A1:K1");
+        const r = ws.range("A1:F1");
         r.merged(true);
         r.value(sheetName);
         r.style({horizontalAlignment: "center", verticalAlignment : "center"});
@@ -164,18 +173,13 @@ export default {
         // set column width, it can be auto adjust with calculate max of data
         ws.column("A").width(20);
         ws.column("B").width(25);
-        ws.column("C").width(15);
-        ws.column("D").width(10);
-        ws.column("E").width(10);
-        ws.column("F").width(20);
-        ws.column("G").width(25);
-        ws.column("H").width(25);
-        ws.column("I").width(10);
-        ws.column("J").width(20);
-        ws.column("K").width(40);
+        ws.column("C").width(25);
+        ws.column("D").width(15);
+        ws.column("E").width(50);
+        ws.column("F").width(100);
 
         // create data from array of json object to array of array
-        var valueArray = this.studentList.map(
+        var valueArray = this.teamList.map(
           item => {
             var va = [];
             header.forEach(element => {
