2a5ba3e4e2634bf08ad4aa1efcbf8eb0f8acaf11
lixueqingsd <xqli@sdu.edu.cn>
2020-05-08 19:23:15 +0800

    lxq

---
+++ src/views/instructor/collegeMaintainStudentInstructor.vue
@@ -0,0 +1,274 @@
+<template>
+  <div class="app-container">
+      <div class="query-container">
+        学生类型
+        <el-select v-model="stuTypeCode" @change="changeMajor" placeholder="请选择学生类型"  style="width: 8%;">
+          <el-option
+            v-for="item in stuTypeList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        专业
+        <el-select v-model="majorId" placeholder="请选择专业"  style="width: 12%;">
+          <el-option
+            v-for="item in majorList"
+            :key="item.majorId"
+            :label="item.majorName"
+            :value="item.majorId">
+          </el-option>
+        </el-select>
+        年级
+        <el-select v-model="grade" placeholder="请选择年级"  style="width: 8%;">
+          <el-option
+            v-for="item in gradeList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        学号
+        <el-input v-model="perNum" placeholder="请输入学号" style="width: 10%;"  />
+        姓名
+        <el-input v-model="perName" placeholder="请输入姓名" style="width: 10%;"  />
+        <el-button type="primary" @click="doQuery" >查询</el-button>
+      </div>
+      <div class="table-container">
+        <el-table
+          :data="studentList"
+          border
+          style="width: 100%;"
+          size="mini"
+        >
+          <el-table-column
+            label="序号"
+            fixed="left"
+            width="50"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              {{ scope.$index+1 }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="学号"
+            align="center"
+            color="black"
+            width="120"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.perNum }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="姓名"
+            align="center"
+            color="black"
+            width="70"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.perName }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="学生类型"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.stuTypeName }}
+            </template>
+          </el-table-column>
+        <el-table-column
+          label="导师"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            <el-select v-if="scope.row.isDoctor === true " v-model="scope.row.tutorId" placeholder="请选择">
+              <el-option
+                v-for="item in doctorTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+            <el-select v-else v-model="scope.row.tutorId" placeholder="请选择">
+              <el-option
+                v-for="item in masterTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="合作导师一"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            <el-select v-if="scope.row.isDoctor === true " v-model="scope.row.tutorId1" placeholder="请选择">
+              <el-option
+                v-for="item in doctorTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+            <el-select v-else v-model="scope.row.tutorId1" placeholder="请选择">
+              <el-option
+                v-for="item in masterTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="合作导师二"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            <el-select v-if="scope.row.isDoctor === true " v-model="scope.row.tutorId2" placeholder="请选择">
+              <el-option
+                v-for="item in doctorTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+            <el-select v-else v-model="scope.row.tutorId2" placeholder="请选择">
+              <el-option
+                v-for="item in masterTutorList"
+                :key="item.personId"
+                :label="item.perNumName"
+                :value="item.personId">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        </el-table>
+      </div>
+    <div align="center">
+      <tr>
+        <td>
+          <el-button type="primary" @click="submitTableData">提交</el-button>
+          <el-button>
+          <a href="/downloads/tutor/tutuorAndStuInfoList.xls" >导入模板下载</a>
+          </el-button>
+          <fileupload
+            url="/api/tutor/importDegreeCollegeStuAndTutorData"
+            :data="{'docType': xls }"
+            accepttype=".xls"
+            @successcallback="onSuccess"
+            style="float: right"
+            @preview="onPreview"
+          >学生导师关系导入
+          </fileupload>
+          </td>
+        </tr>
+    </div>
+  </div>
+</template>
+Init
+<script>
+import { collegeMaintainStudentTutorInit } from '@/api/tutor'
+import { collegeMaintainStudentTutorMajor} from '@/api/tutor'
+import { collegeMaintainStudentTutorQuery} from '@/api/tutor'
+import { collegeMaintainStudentTutorSubmit} from '@/api/tutor'
+import fileupload from '../../components/upload/fileupload'
+export default {
+  name: 'CollegeMaintainStudentTutor',
+  components: { fileupload },
+  data() {
+    return {
+      stuTypeCode:'',
+      majorId:-1,
+      grade:'',
+      perNum:'',
+      perName:'',
+      stuTypeList:[],
+      gradeList:[],
+      majorList:[],
+      doctorTutorList:[],
+      masterTutorList:[],
+      studentList:[]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      collegeMaintainStudentTutorInit({ 'session': document.cookie }).then(res => {
+        this.studentList = res.data.studentList
+        this.stuTypeList = res.data.stuTypeList
+        this.gradeList = res.data.gradeList
+        this.majorList = res.data.majorList
+        this.doctorTutorList = res.data.doctorTutorList
+        this.masterTutorList=res.data.masterTutorList
+      })
+    },
+    doQuery(){
+      collegeMaintainStudentTutorQuery({ 'session': document.cookie, 'stuTypeCode': this.stuTypeCode,
+        'grade': this.grade, 'majorId': this.majorId, 'perNum': this.perNum, 'perName': this.perName
+      }).then(res => {
+        this.studentList = res.data
+      })
+    },
+    changeMajor(){
+      collegeMaintainStudentTutorMajor({ 'session': document.cookie, 'stuTypeCode': this.stuTypeCode
+      }).then(res => {
+        this.majorList = res.data
+      })
+    },
+    submitTableData() {
+      console.log(this.studentList)
+      collegeMaintainStudentTutorSubmit({ 'session': document.cookie, 'studentList': this.studentList }).then(res => {
+        console.log(res);
+        if(res.code === '0'){
+          this.$message({
+            message: '提交成功',
+            type: 'success'
+          });
+          this.fetchData()
+        }
+        else{
+          this.$message({
+            message: res.msg,
+            type: 'error'
+          });
+        }
+      })
+    },
+    onPreview: function(file) {
+    },
+    onSuccess(res, file) {
+        if(res.code === '0'){
+          this.$message({
+            message: '导入成功',
+            type: 'success'
+          });
+          this.fetchData()
+        }
+        else{
+          this.$message({
+            message: res.msg,
+            type: 'error'
+          });
+        }
+
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/instructor/studentInstructorCollegeMemberManage.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="app-container">
+      <div class="query-container">
+      类别
+      <el-select v-model="memberType" placeholder="请选择类别" class="filter-item" style="width: 20%;">
+        <el-option
+          v-for="item in typeList"
+          :key="item.memberType"
+          :label="item.memberName"
+          :value="item.memberType">
+        </el-option>
+      </el-select>
+      <el-button type="primary" @click="doQuery" >查询</el-button>
+      </div>
+    <div>
+      <div class="table-container">
+        <el-table
+          :data="memberList"
+          border
+          style="width: 100%;"
+          size="mini"
+        >
+          <el-table-column
+            label="序号"
+            fixed="left"
+            width="50"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              {{ scope.$index+1 }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="工号"
+            align="center"
+            color="black"
+            width="120"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.perNum }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="姓名"
+            align="center"
+            color="black"
+            width="70"
+          >
+            <template slot-scope="scope">
+              <el-button type="text" @click="showPersonInfo(scope.row.personId)" >{{ scope.row.perName }}</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="职称"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.proTechPositionName }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="学院"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.collegeName }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            align="center"
+            color="black"
+          >
+            <template slot-scope="scope">
+              <el-button type="primary" @click="deletePerson(scope.row.memberId)"  >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+
+      <div align="center">
+        <el-button type="primary" @click="memberClear(memberType)" >清空</el-button>
+        <el-button type="primary" @click="memberInit(memberType)" >初始</el-button>
+        <el-button type="primary" @click="addPerson(memberType)" >添加</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { degreeCollegeTutorMemberManageInit } from '@/api/tutor'
+import { degreeCollegeTutorMemberQuery} from '@/api/tutor'
+import { degreeCollegeTutorMemberPersonDelete } from '@/api/tutor'
+import { degreeCollegeTutorMemberClear } from '@/api/tutor'
+import { degreeCollegeTutorMemberInit } from '@/api/tutor'
+export default {
+  name: 'DegreeCollegeTutorMemberManage',
+  data() {
+    return {
+      memberType:'',
+      typeList:[],
+      memberList:[]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      degreeCollegeTutorMemberManageInit({ 'session': document.cookie }).then(res => {
+        this.memberList = res.data.memberList
+        this.typeList = res.data.typeList
+        this.memberType=res.data.memberType
+      })
+    },
+    doQuery(){
+      degreeCollegeTutorMemberQuery({ 'session': document.cookie, 'memberType': this.memberType
+      }).then(res => {
+        this.memberList = res.data.memberList
+      })
+    },
+    deletePerson(memberId){
+       degreeCollegeTutorMemberPersonDelete({ 'session': document.cookie, 'memberId': memberId}).then(res => {
+         this.fetchData()
+      })
+    },
+    memberClear(memberType){
+        this.$confirm('此操作将清除导师组中所有成员, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+		      degreeCollegeTutorMemberClear({ 'session': document.cookie, 'memberType': memberType}).then(res => {
+          this.fetchData()
+        })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消清除'
+          });
+        });
+    },
+    memberInit(memberType){
+       degreeCollegeTutorMemberInit({ 'session': document.cookie, 'memberType': memberType}).then(res => {
+         this.fetchData()
+      })
+    },
+    addPerson(memberType){
+      console.log(memberType);
+      this.$router.push({ path: 'degreeCollegeTutorMemberManageAdd', query: { memberType }})
+    },
+    showPersonInfo(personId){
+      this.$router.push({ path: '/tutor/tutorDetailInfoShow', query: { personId }})
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
