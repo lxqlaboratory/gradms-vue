1f7fb57bc64a10f8219c0e2258283b28019bb091
lixueqingsd <xqli@sdu.edu.cn>
2020-05-14 15:29:19 +0800

    Update recruitBookSourceMaintain.vue

--- src/views/tutor/recruitBookSourceMaintain.vue
+++ src/views/tutor/recruitBookSourceMaintain.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="app-container">
+      <table class="content"  align="center">
+      <tr>
+        <td  style="font-size: 16px;color: red;text-align:left; ">
+          说明:<br>
+          &nbsp;&nbsp;1.个人专著维护主要用于自己专著库的维护，作者科自行添加发表的专著，添加后请所在医院进行审核后可可以在申请专著维护的候选列表中出现<br>
+          &nbsp;&nbsp;2.医院审核通过可以编辑修改和删除，审核后将不能在编辑修改和删除。<br>
+          &nbsp;&nbsp;3.编辑修改专著信息修改保存后可以上传附件，方便医院审核。
+        </td>
+        <td  style="font-size: 16px;color: red;text-align:left; ">
+          说明:<br>
+          &nbsp;&nbsp;该功能目前仅限于用于临床医院的博导、硕导科研成果库的维护，其他导师无法使用<br>
+        </td>
+      </tr>
+    </table>
     <el-table
       :data="dataList"
       border
