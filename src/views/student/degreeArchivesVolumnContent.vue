1127208943cfc15b6242ebaad60cc2c646a0fba7
lixueqingsd <xqli@sdu.edu.cn>
2020-09-03 20:11:11 +0800

    lxq

--- src/views/student/degreeArchivesVolumnContent.vue
+++ src/views/student/degreeArchivesVolumnContent.vue
@@ -136,7 +136,7 @@ export default {
           window.location.href = this.serverAddres+ '/archives/archives_initPrintInfo.do?personId=' + personId
     },
     doPrint(personId){
-          window.location.href = this.serverAddres+ '/archives/archives_printLetter.do?exportName=archivesPrint&&exportType=1094&&doctype=pdf&&fileName=juanneimulu.pdf&personId='+personId
+          window.location.href = this.serverAddres+ '/archives/archives_printLetter.do?exportName=archivesPrint&exportType=1094&&doctype=pdf&fileName=juanneimulu.pdf&personId='+personId
     },
     doAdd(){
           window.location.href = this.serverAddres+ '/archives/archives_initAddInfo.do?degreeDate='+this.degreeDate+'&stuType='+this.stuTypeCode
